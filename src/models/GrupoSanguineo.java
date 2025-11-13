c846f4e419e74c67f3a8fe033a3c264b3c617b53
JuampiRibero <juampiribero@gmail.com>
2025-11-13 20:23:22 -0300

    Change in GrupoSanguineo.java file.

--- src/models/GrupoSanguineo.java
+++ src/models/GrupoSanguineo.java
@@ -1,5 +1,16 @@
 package models;
 
 public enum GrupoSanguineo {
-    A_POS, A_NEG, B_POS, B_NEG, AB_POS, AB_NEG, O_POS, O_NEG;
+    A_POS("A+"), A_NEG("A-"), B_POS("B+"), B_NEG("B-"), AB_POS("AB+"), AB_NEG("AB-"), O_POS("O+"), O_NEG("O-");
+    
+    private final String simbolo;
+    
+    GrupoSanguineo(String simbolo) {
+        this.simbolo = simbolo;
+    }
+    
+    @Override
+    public String toString() {
+        return simbolo;
+    }
 }
